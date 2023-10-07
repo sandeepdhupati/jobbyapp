6b6866161290456a8ccfcae17705a0695be428b7
sandeep <sandeepdhupati@gmail.com>
2023-10-07 10:11:23 +0000

    show

---
+++ src/JobItem/index.js
@@ -0,0 +1,63 @@
+import {Link} from 'react-router-dom'
+import {MdLocationOn} from 'react-icons/md'
+import {BsFillBriefcaseFill, BsStarFill} from 'react-icons/bs'
+
+import './index.css'
+
+const JobItem = props => {
+  const {jobData} = props
+  const {
+    companyLogoUrl,
+    employmentType,
+    id,
+    jobDescription,
+    location,
+    packagePerAnnum,
+    rating,
+    title,
+  } = jobData
+  return (
+    <Link to={`/jobs/${id}`} className="link-item">
+      <li className="job-item-container">
+        <div className="first-part-container">
+          <div className="img-title-container">
+            <img
+              className="company-logo"
+              src={companyLogoUrl}
+              alt="company logo"
+            />
+            <div className="title-rating-container">
+              <h1 className="title-heading">{title}</h1>
+              <div className="star-rating-container">
+                <BsStarFill className="star-icon" />
+                <p className="rating-text">{rating}</p>
+              </div>
+            </div>
+          </div>
+          <div className="location-package-container">
+            <div className="location-job-type-container">
+              <div className="location-job-type-icon-container">
+                <MdLocationOn className="location-job-icon" />
+                <p className="location">{location}</p>
+              </div>
+              <div className="location-job-type-icon-container">
+                <BsFillBriefcaseFill className="location-job-icon" />
+                <p className="job-type">{employmentType}</p>
+              </div>
+            </div>
+            <div>
+              <p className="package">{packagePerAnnum}</p>
+            </div>
+          </div>
+        </div>
+        <hr className="item-hr-line" />
+        <div className="second-part-container">
+          <h1 className="description-heading">Description</h1>
+          <p className="description-para">{jobDescription}</p>
+        </div>
+      </li>
+    </Link>
+  )
+}
+
+export default JobItem
